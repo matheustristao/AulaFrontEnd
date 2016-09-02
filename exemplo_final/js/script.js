d096b3f398a22b73f6a93224e67f0e5a1ed44345
Matheus Oliveira Tristão dos Anjos <matheusanjos@unasus.gov.br>
2016-09-02 15:54:01 -0300

    main finalizada

--- exemplo_final/js/script.js
+++ exemplo_final/js/script.js
@@ -2,7 +2,22 @@ $(document).ready(function () {
 
     $("#mobileMenu").click(function () {
         $('.ui.labeled.icon.sidebar')
-                .sidebar('toggle')
-                ;
+                .sidebar('toggle');
     });
+
+    $("figure").click(function () {
+        $(this).transition("pulse");
+    });
+
+    $("#btnVerde").click(function () {
+        $("#item1")
+                .transition({
+                    animation: 'fly left',
+                    duration: '1s',
+                    onComplete: function () {
+                      console.log("acionado");
+                    }
+                });
+    });
+    
 });
